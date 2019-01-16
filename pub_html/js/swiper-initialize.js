f5671bb5fc429374a22b99744f9b735cc096d353
Unknown <sarahtong94@hotmail.com>
2019-01-16 08:15:59 -0800

    Fixed the arrows of the swiper on the SMA page. About page done.

--- pub_html/js/swiper-initialize.js
+++ pub_html/js/swiper-initialize.js
@@ -1,4 +1,4 @@
-var mySwiper = new Swiper('.swiper-container', {
+var homepageSwiper = new Swiper('.swiper-container', {
 
     loop: false,
     slidesPerView: 4,
@@ -62,7 +62,23 @@ var mySwiper = new Swiper('.swiper-container', {
 
 });
 
-var mySwiper = new Swiper('.swiper-container-project', {
+var SMABrandingBookSwiper = new Swiper('#SMA-branding-book', {
+    loop: false,
+    slidesPerView: 1,
+    spaceBetween: 80,
+    
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
+
+var SMABrochureSwiper = new Swiper('#SMA-brochure', {
     loop: false,
     slidesPerView: 1,
     spaceBetween: 80,
